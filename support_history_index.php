d6b5edf0402a2b8f59fde72237bae49242d44b5f
nacfkhan <73815672+nacfkhan@users.noreply.github.com>
2020-11-09 17:24:23 +0900

    Create support_history_index.php

---
+++ support_history_index.php
@@ -0,0 +1,214 @@
+
+<head>  
+<title> 농축협 시설보조비 지원 현황 </title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">  
+
+<style type="text/css">
+body
+{
+	background-color:#FFF;
+	margin:0 auto;
+}
+p { margin:0; }
+
+#wrap
+{
+	width:100%;
+	height:100%;
+	margin:0 auto;
+	text-align:center;
+}
+#container
+{
+	width:600px;
+	height:100%;
+	background-color:#FFF;
+	margin:0 auto;
+}
+	.logo
+	{
+	  font-size:20pt;
+	  font-weight:bold;	  
+		text-align:center;
+		padding-top:40px;
+		padding-bottom:40px;
+	}
+	.setup_msg
+	{
+		text-align:center;
+		font-weight:bold;
+		padding-bottom:30px;
+	}
+	.info_msg
+	{
+		font-size:9pt;
+		color:#666;
+		text-align:left;
+		padding-left:10px;
+	}
+
+.info_table td
+{
+	font-size:9pt;
+	padding-top:8px;
+	padding-bottom:5px;
+	color:#666;
+}
+
+.info_table2 td
+{
+	font-size:9pt;
+	padding-top:8px;
+	padding-bottom:5px;
+  padding-left:0px;
+	color:#666;
+}
+
+	.info_category
+	{
+ 		text-align:center;
+		background-color:#ececec;
+		padding-left:10px;
+	}
+  .info_category2
+	{
+ 		text-align:right;
+		background-color:#ececec;
+		padding-left:10px;
+	}
+  .info_category3
+	{
+ 		text-align:left;
+		background-color:#ececec;
+		padding-left:10px;
+	}
+	.info_space
+	{
+ 		text-align:center;
+		background-color:#FF9966;
+		padding-left:5px;
+	}
+	.info_bg
+	{
+	  text-align:center;
+		background-color:#FFF;
+		padding-left:10px;
+	}
+		.info_bg2
+	{
+	  text-align:left;
+		background-color:#FFF;
+		padding-left:10px;
+	}
+		.info_bg3
+	{
+	  text-align:right;
+		background-color:#FFF;
+		padding-left:10px;
+	}
+
+	.go_home
+	{
+		text-align:center;
+		padding-top:20px;
+		padding-bottom:20px;
+	}
+
+	.copy
+	{
+		background-color:#f3f3f3;
+		padding-top:10px;
+		height:35px;
+		font-family:verdana,tahoma;
+		text-align:center;
+		font-size:8pt;
+	}
+
+.c_00 { color:#000; }
+.c_red { color:#9e0b0f; }
+
+.p_b10 { padding-bottom:10px; }
+</style>
+    </head> 
+
+<script type="text/javascript">
+
+function click()
+{
+  if ((event.button==2) || (event.button==2)) 
+  {
+    alert('죄송합니다. 오른쪽 마우스 금지입니다. ^^;;; ');
+  }
+}document.onmousedown=click// -->
+
+</script>
+
+<body  oncontextmenu='return false' ondragstart='return false' onselectstart='return false'>
+
+<br>
+<br>
+<br>
+<br>
+
+ 
+<table cellpadding="0" cellspacing="0" border="0" width="100%"  bgcolor="#FF9966" class="info_bg">
+ <tr>
+   <td>
+      <h3  class="logo" >[ 농축협 시설보조비 지원 현황 ]</h3>
+   </td>   
+ </tr>
+</table>
+
+<table cellpadding="0" cellspacing="5" border="0" width="100%"  bgcolor="#FF9966" class="info_space">
+<tr> </tr>
+</table>  
+
+<br>
+
+ <form Action="nonghyup_user_info.php" method="post">   
+
+   <table cellpadding="0" cellspacing="0" border="0" width="100%"  bgcolor="#d7d7d7" class="info_table2">
+     <tr>
+        <td width="50%" class="info_bg3">개인번호 : </td>
+        <td class="info_bg2">&nbsp; <input type="text"   name="user_id"       size="20">
+                                    <input type="hidden" name="master_gubun" value="1">
+        </td>
+     </tr>
+   </table>  
+
+   <table cellpadding="0" cellspacing="0" border="0" width="100%"  bgcolor="#d7d7d7" class="info_table2">
+     <tr><td width="100%" class="info_bg">&nbsp;</td></tr>  
+   </table>
+   
+   <table cellpadding="0" cellspacing="0" border="0" width="100%"  bgcolor="#d7d7d7" class="info_table2">  
+     <tr>
+        <td width="55%" class="info_bg3">
+        <button type="submit" >&nbsp;&nbsp;확 인&nbsp;&nbsp;</button>
+        <!-- <input type="button" name="Button" value="확 인" onClick="newWindow();"> -->
+        </td>
+         </form>  
+        <td   class="info_bg2">&nbsp;</td>
+        <td   class="info_bg2">
+        <form Action="index.php" method="post">
+        <button type="submit">&nbsp;&nbsp;취 소&nbsp;&nbsp;</button>
+        </form>
+        </td>
+     </tr>
+   </table>  
+
+		<div class="info_msg">
+   		<p class="p_b09">&nbsp;</p>
+		</div>
+
+<table cellpadding="0" cellspacing="5" border="0" width="100%"  bgcolor="#FF9966" class="info_space">
+<tr> </tr>
+</table>  
+
+
+ </body>  
+
+
+<br>
+<br>
+
+   
